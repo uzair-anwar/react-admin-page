16d5a5698221c20521c2b9f7b7e2e81f156b1e04
uzair-anwar <uzair.anwar@devsinc.com>
2023-01-31 16:12:05 +0500

    Solved version problem and color bug in sidebar
    zq

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import { useState } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+
 import "./App.css";
+import TopBar from "./Pages/global/TopBar";
+import SideBar from "./Pages/global/SideBar";
+import { ProSidebarProvider } from "react-pro-sidebar";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./Pages/dashboard/Dashboard";
 
 function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
   return (
-    <div className="App">
-      <h1>Admin Board</h1>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <SideBar isSidebar={isSidebar} />
+          <main className="content">
+            <TopBar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
